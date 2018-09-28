3d010034f00bcae5e1d1151560d979820572cc18
meow <ahmedbilal96@gmail.com>
2018-09-28 20:59:47 +0500

    Initial code added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Video-Summarization
+# Video Summarization
+As video content is growing rapidly where video summarization helps us to focus on automatically selecting informative parts from video, is becoming critical. In our project we are going to develop a dynamic video synopsis(simultaneous presentation of events that enables the review of hours of video footage in just minutes).We can do that by shifting events from their original time interval to another time interval when no other activity takes place at that spatial location. There are two approaches that were commonly used for video synopsis, first one is to select a set of key frames from the original video sequence and second is a collection of short video sequences. Dynamic video synopsis has lot of applications in real life.
+
+
+## References
+1. https://en.wikipedia.org/wiki/Video_synopsis
+2. http://www.vision.huji.ac.il/video-synopsis/iccv07-webcam.pdf
+3. http://www.vision.huji.ac.il/video-synopsis/pami08-synopsis.pdf
+4. http://www.vision.huji.ac.il/video-synopsis/cvpr06-synopsis.pdf
---
+++ getting_started/image processing/substantial_motion_video.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def diff_perc(image1, image2):
+    gray_image1 = cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY)
+    gray_image2 = cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY)
+    diff = cv2.absdiff(gray_image1,gray_image2)
+    return np.sum(diff)/np.prod(diff.shape)
+
+
+def substantial_motion_video(filename, out_filename):
+    THRESHOLD = 0.5
+    ADJUSTMENT = 0.7
+    frames = []
+    substantially_diff_frames = []
+
+    video = cv2.VideoCapture(filename)
+    frames_per_sec_in_org_video = video.get(cv2.CAP_PROP_FPS)
+    success, image = video.read()
+    while success:
+        frames.append(image)
+        success, image = video.read()
+
+    substantially_diff_frames.append(frames[0])
+
+    for frame in frames:
+        diff = diff_perc(frame, substantially_diff_frames[-1])
+        if diff > THRESHOLD:
+            substantially_diff_frames.append(frame)
+    video = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc(*'H264'),
+                            frames_per_sec_in_org_video * ADJUSTMENT, tuple(reversed(substantially_diff_frames[0].shape[:2])))
+
+    for frame in substantially_diff_frames:
+        video.write(frame)
+    video.release()
+
+    del frames
+    del substantially_diff_frames
+
+# substantial_motion_video(input_filename, output_filename)
+
