7071f5395b0247c34aa90e2ab8ed3f5f7082c1ee
meow <ahmedbilal96@gmail.com>
2018-11-10 11:05:27 +0500

    Rename ABKVideoCapture to BATRVideoCapture and Added BATRPickle

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import os
+import pickle
+import tarfile
+import zlib
 
 import cv2
 import numpy as np
@@ -7,6 +10,7 @@ import numpy as np
 def diff_perc(image1, image2):
     """
         Author: Ahmed Bilal Khalid
+        Contributor: None
 
         Returns absolute difference of image1 and image2 in percentage
     """
@@ -15,25 +19,41 @@ def diff_perc(image1, image2):
     diff = cv2.absdiff(gray_image1, gray_image2)
     return np.sum(diff)/np.prod(diff.shape)
 
-
-class ABKVideoCapture(object):
+class BATRVideoCapture(object):
     """
         Author: Ahmed Bilal Khalid
+        Contributor: None
     """
 
-    def __init__(self, file):
+    def __init__(self, file, offset=0):
         if os.path.isfile(file):
             self.video = cv2.VideoCapture(file)
             self.frames = self.frames_generator()
             self.frames_per_second = self.video.get(cv2.CAP_PROP_FPS)
             self.total_frames = self.video.get(cv2.CAP_PROP_FRAME_COUNT)
             self.dimensions = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH)),int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            self.video.set(cv2.CAP_PROP_POS_FRAMES, offset-1)
         else:
             raise FileNotFoundError()
 
+    def set_offset(self, offset):
+        self.video.set(cv2.CAP_PROP_POS_FRAMES, offset-1)
+
+    def set_offset_in_second(self, second):
+        """
+        Author: Ahmed Bilal Khalid
+        Acknowledgement: https://stackoverflow.com/a/47867180
+
+        Offset VideoCapture object by argument (second)
+        Note: It uses some non-documented stuff. So, use it on your own risk
+        """
+
+        self.video.set(cv2.CAP_PROP_POS_FRAMES, second * self.frames_per_second - 1)
+    
     def frames_generator(self):
         """
         Author: Ahmed Bilal Khalid
+        Contributor: None
 
         Returns next frame from self.video"""
 
@@ -48,17 +68,20 @@ class ABKVideoCapture(object):
         """
         Author: Ahmed Bilal Khalid
 
-        Returns next frame from self.frames"""
+        Returns next frame from self.frames
+        """
 
         return next(self.frames)
 
     def create_substantial_motion_video(self, out_filename, fourcc_str="avc1"):
         """
         Author: Ahmed Bilal Khalid
+        Contributor: None
 
-        Creates a new svideo which only consists of those frames from original
+        Creates a new video which only consists of those frames from original
         video that have substantial motion as compared to previously selected
-        frame."""
+        frame.
+        """
 
         THRESHOLD = 0.5
 
@@ -85,3 +108,67 @@ class ABKVideoCapture(object):
         background = background / self.total_frames
 
         return background.astype(np.uint8)
+
+
+class BATRPickle(object):
+    def __init__(self, in_file = None, out_file = None):
+        self.input_file = None
+        self.output_file = None
+
+        if in_file:
+            self.input_file = tarfile.open(in_file, "r:bz2")
+        
+        if out_file:
+            self.output_file = tarfile.open(out_file, "w:bz2")
+
+    def pickle(self, obj, filename):
+        """
+        Author: Ahmed Bilal Khalid
+        Contributor: None
+
+        Compress obj using zlib and add that compressed obj to self.output_file
+        """
+
+        serialized_obj = pickle.dumps(obj, pickle.HIGHEST_PROTOCOL)
+        compressed = zlib.compress(serialized_obj, 9)
+        with open(filename, "wb") as f:
+            f.write(compressed)        
+        self.output_file.add(filename)
+        os.remove(filename)
+
+    
+    def unpickle(self):
+        """
+        Author: Ahmed Bilal Khalid
+        Contributor: None
+
+        Return an iterator to uncompressed and unpickled file member of self.input_file
+        """
+
+        member = self.input_file.next()
+
+        while member:
+            f = self.input_file.extractfile(member)
+            content = f.read()
+            uncompressed = zlib.decompress(content)
+            member = self.input_file.next()
+            yield pickle.loads(uncompressed)
+
+
+    def __del__(self):
+        """
+        Author: Ahmed Bilal Khalid
+        Contributor: None
+
+        Close open compressed files
+        """
+
+        if self.input_file:
+            self.input_file.close()
+        
+        if self.output_file:
+            self.output_file.close()
+
+
+# For API Compatibility
+ABKVideoCapture = BATRVideoCapture
